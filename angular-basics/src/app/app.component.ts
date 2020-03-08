ac91fe87d4eb7bdde759cd9f606a6257d70f9526
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-08 16:33:33 +0200

    binding - typeScript with html []

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,9 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   // title = 'angular-basics';
   title = 'Dynamic title';
   number = '42';
   arr = [1, 2, 3];
   obj = {a: 1, b: {c: 2} };
+
+  img = 'https://b7.pngbarn.com/png/78/799/react-redux-javascript-library-node-js-facebook-react-png-clip-art-thumbnail.png';
+
+  constructor() {
+    setTimeout(() => {
+      console.log('timeout is over');
+      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
+    }, 5000);
+  }
 }

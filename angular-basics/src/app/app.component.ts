3cf9e3a096a9a49566aa47498a2922e6473cc409
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-08 18:59:56 +0200

    Event binding - typeScript with typeSctip ()

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -7,18 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // title = 'angular-basics';
-  title = 'Dynamic title';
-  number = '42';
+  title = 'angular-basics';
+  // title = 'Dynamic title';
+  number = 42;
   arr = [1, 2, 3];
   obj = {a: 1, b: {c: 2} };
 
-  img = 'https://b7.pngbarn.com/png/78/799/react-redux-javascript-library-node-js-facebook-react-png-clip-art-thumbnail.png';
+  inputValue =  '';
+
+// binding typeScript with html []
+  // img = 'https://b7.pngbarn.com/png/78/799/react-redux-javascript-library-node-js-facebook-react-png-clip-art-thumbnail.png';
 
   constructor() {
-    setTimeout(() => {
-      console.log('timeout is over');
-      this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
-    }, 5000);
+    //   setTimeout(() => {
+    //     console.log('timeout is over');
+    //     this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
+    //   }, 5000);
   }
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str
+  }
+
+  onClick() {
+    console.log('Click!');
+  }
+
 }

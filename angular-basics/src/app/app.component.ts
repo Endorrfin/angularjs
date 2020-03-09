63be958091f0b9164e9527217d74efd1856bbabe
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-09 13:40:32 +0200

    Created 2 components post & post-form

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -7,27 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  // backgroundToggle = false;
-
-  // toggle: any = false;
-
-  // arr = [1, 1, 2, 3, 5, 8, 13];
-
-  // objs = [
-  //   {title: 'Post 1', author: 'Jack London', comments: [
-  //       {name: 'Max', text: 'lorem 1'},
-  //       {name: 'Max', text: 'lorem 2'},
-  //       {name: 'Max', text: 'lorem 3'},
-  //     ]},
-  //   {title: 'Post 1', author: 'Agata Kristy', comments: [
-  //       {name: 'Pobin', text: 'lorem 1'},
-  //       {name: 'Pobin', text: 'lorem 2'},
-  //       {name: 'Pobin', text: 'lorem 3'},
-  //     ]},
-  // ]
-
-  now: Date = new Date()
-
 
 
 

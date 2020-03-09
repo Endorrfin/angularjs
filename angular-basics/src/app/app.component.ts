fbd2990c99f959ea60275bd3dbf3b2d4842bab00
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-09 09:59:09 +0200

    2 way Binding

--- angular-basics/src/app/app.component.ts
+++ angular-basics/src/app/app.component.ts
@@ -6,35 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  title = '';
 
-  title = 'angular-basics';
-  // title = 'Dynamic title';
-  number = 42;
-  arr = [1, 2, 3];
-  obj = {a: 1, b: {c: 2} };
-
-  inputValue =  '';
-
-// binding typeScript with html []
-  // img = 'https://b7.pngbarn.com/png/78/799/react-redux-javascript-library-node-js-facebook-react-png-clip-art-thumbnail.png';
-
-  constructor() {
-    //   setTimeout(() => {
-    //     console.log('timeout is over');
-    //     this.img = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/240px-Angular_full_color_logo.svg.png';
-    //   }, 5000);
+  onInput(event: any) {
+    this.title = event.target.value
   }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
-
-  onClick() {
-    console.log('Click!');
-  }
-
 }

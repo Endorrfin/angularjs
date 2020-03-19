a586d1bc953ea350e319209fa6a29658337d9e8b
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-19 23:11:46 +0200

    Display a List of Heroes

--- tourOfHeroes/src/app/heroes/heroes.component.ts
+++ tourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from "./hero";
+import { Hero } from "./hero";
+import { HEROES } from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,15 @@ import {Hero} from "./hero";
 })
 export class HeroesComponent implements OnInit {
 
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  public selectedHero: Hero;
+
+  // зона видимости TypeScript - указываем, что heroes - это массив класса Hero
+  public heroes: Hero[] = HEROES;
+
+  // метод onSelect для функционала клика в списке героев
+  public onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 
 

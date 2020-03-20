83c8545c7d6e94e1109c6d828e1e3f5c009e55c1
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-20 17:09:26 +0200

    Angular ToDo my dream app: done - markup, ability add task & remove of task

--- angularTodoDreamApp/src/app/app.component.ts
+++ angularTodoDreamApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularTodoDreamApp';
+  title = 'Angular ToDo my dream app';
 }

b561dbd8701147612cb7bf25a292e8cf1e74a47b
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-21 19:58:54 +0200

    added in app navigations

--- tourOfHeroes/src/app/heroes/heroes.component.ts
+++ tourOfHeroes/src/app/heroes/heroes.component.ts
@@ -7,30 +7,19 @@ import { HeroService } from "../hero.service";
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-  public selectedHero: Hero;
-
-  // зона видимости TypeScript - указываем, что heroes - это массив класса Hero
-  public heroes: Hero[];
-
-  // метод onSelect для функционала клика в списке героев
-  // selectedHero - герой на которого кликнули
-  public onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  // heroService - имя переменной, которое придумываем самостоятельно, но чаще все принято использовать
-  // имя аналогичное имени сервиса но с маленькой буквы.
   constructor(private heroService: HeroService) { }
 
-  private getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 }
+

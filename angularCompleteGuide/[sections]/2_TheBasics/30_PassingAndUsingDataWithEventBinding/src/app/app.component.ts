de162d4b1bdf52e306c143e6ea86c6a661beb13a
vasiliy.k <vasiliy.k@payop.com>
2020-04-20 11:57:35 +0300

    Angular Complete Guide: Section 2: The Basics

--- angularCompleteGuide/[sections]/2_TheBasics/30_PassingAndUsingDataWithEventBinding/src/app/app.component.ts
+++ angularCompleteGuide/[sections]/2_TheBasics/30_PassingAndUsingDataWithEventBinding/src/app/app.component.ts
@@ -12,16 +12,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Vasiliy';
-  username = '';
-  showSecret = false;
-  log = [];
-
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    // this.log.push(this.log.length + 1);
-    this.log.push(new Date());
-  }
 }
-
-
--- angularCompleteGuide/[sections]/2_TheBasics/30_PassingAndUsingDataWithEventBinding/src/app/app.module.ts
+++ angularCompleteGuide/[sections]/2_TheBasics/30_PassingAndUsingDataWithEventBinding/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from "./components/warning-alert/warning-alert-component";
-import { SuccessAlertComponent } from './components/success-alert/success-alert.component';
+import {WarningAlertComponent} from "./warning-alert/warning-alert-component";
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [

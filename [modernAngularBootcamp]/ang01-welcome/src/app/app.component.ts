78a632b80f65754e0501b52ba2a848c18401382d
Vasiliy Krupka <vasiliy.k@payop.com>
2020-08-19 11:56:45 +0300

    update

--- [modernAngularBootcamp]/ang01-welcome/src/app/app.component.ts
+++ [modernAngularBootcamp]/ang01-welcome/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "./translate.service";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang01-welcome';
+  inputText: string;
+  translatedText: string
+  constructor (private translateService: TranslateService) {}
+
+  submit() {
+    this.translateService.translate(this.inputText).subscribe((result) => {
+      this.translatedText = result;
+    })
+  }
 }
